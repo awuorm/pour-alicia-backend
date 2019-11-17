224600e884d43350ab5b7d474d44f654db8420da
Mildred Awuor <otieno.awuor.m@gmail.com>
2019-11-17 19:42:10 +0300

    edited seeds

--- data/seeds/01-users-seeds.js
+++ data/seeds/01-users-seeds.js
@@ -5,7 +5,7 @@ exports.seed = function(knex) {
         {   id:1,
           title:"Is There Anyone Like Lorem Ipsum?",
           author:"Created by Lorem Ipsum",
-          storybody: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          storybody: "Lorem ipsum dolor sit amet"
       }
         
       ]);
