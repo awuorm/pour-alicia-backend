596831e3c7b650a02217e7b150f651d43ca958fe
Mildred Awuor <otieno.awuor.m@gmail.com>
2019-11-17 13:55:43 +0300

    added login endpoint

--- auth/authModel.js
+++ auth/authModel.js
@@ -2,9 +2,13 @@ const db = require("../data/dbConfig");
 
 module.exports = {
     add,
-    // findBy
+    findBy
 }
 
 function add(user) {
    return db("users").insert(user);
+}
+
+function findBy(username) {
+    return db("users").where({username}).first();
 }
